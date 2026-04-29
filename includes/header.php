4f78fe0c5c2677bee3a3a29d40d35ed80d1d290c
Sammy Opiyo <sammyopiyo001@gmail.com>
2026-04-29 11:08:07 +0300

    feat: add reusable header component with navigation and contact information

--- includes/header.php
+++ includes/header.php
@@ -79,6 +79,12 @@
             </svg>
             <span>Client Login</span>
           </a>
+          <a href="https://www.portal.shanfixtechnology.com" target="_blank" rel="noopener noreferrer" class="top-header-link">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+              <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
+            </svg>
+            <span>Bulk SMS Login</span>
+          </a>
         </div>
       </div>
     </div>
